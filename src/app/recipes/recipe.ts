d25e88981408a8bb7a3c321f8623f824e9ff8395
Jeffrey van Bijleveld <jeffrey.vanbijleveld@ictu.nl>
2023-08-02 17:53:16 +0200

    basic component setup (needs styling tough)

--- src/app/recipes/recipe.ts
+++ src/app/recipes/recipe.ts
@@ -3,5 +3,6 @@ import {Instruction} from "./Instruction";
 export interface Recipe {
   id: number,
   name: string,
+  description?: string,
   instructions: Instruction[]
 }
--- src/app/recipes/recipe/recipe.component.ts
+++ src/app/recipes/recipe/recipe.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {Recipe} from "../recipe";
 import {RecipesService} from "../recipes.service";
 import {ActivatedRoute} from "@angular/router";
+import {Ingredient} from "../Ingredient";
 
 @Component({
   selector: 'app-recipe',
@@ -11,6 +12,7 @@ import {ActivatedRoute} from "@angular/router";
 export class RecipeComponent {
 
   recipe?: Recipe;
+  ingredients?: Ingredient[];
 
   constructor(private service: RecipesService, private route: ActivatedRoute) {
     this.loadData();
@@ -20,8 +22,21 @@ export class RecipeComponent {
     const paramId = this.route.snapshot.paramMap.get('id');
     if (paramId) {
       let recipeId = parseInt(paramId);
-      this.service.getRecipe(recipeId).subscribe(resp => this.recipe = resp);
+      this.service.getRecipe(recipeId).subscribe(resp => {
+        this.recipe = resp;
+        this.ingredients = this.extractIngredients(resp);
+      });
     }
   }
 
+  extractIngredients(recipe:Recipe) : Ingredient[] {
+    let ingredients: Ingredient[] = [];
+    recipe.instructions.forEach( instruction => {
+      instruction.ingredients?.forEach(ing => {
+        ingredients.push(ing);
+      })
+      }
+    );
+    return ingredients;
+  }
 }
